3abc01d9a04d558082bf37f9109896c067332975
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-01-20 19:54:35 -0330

    Complete Quiz Questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,94 +5,15 @@ var firstpageContainerEL = document.querySelector(".firstpage-container");
 var questionContainerEl = document.querySelector(".quiz-container");
 var questionEl = document.querySelector("#question");
 var answerEl = document.querySelector("#answer-btns");
+var responseEl = document.querySelector("#response");
+var highscoreEl = document.querySelector(".quiz-highscore");
 
 // Variable Declarations
-let randomQuestions, currentQuestionIndex;
+let currentQuestionIndex = 0;
 var timeLeft = 75;
 var timeInterval;
 var highscores = [];
 var score = 0;
-
-startButtonEl.addEventListener("click", startGame)
-
-function startGame() {
-    firstpageContainerEL.classList.add("hide");
-    randomQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerEl.classList.remove("hide");
-    setQuizQuestion();
-    countdownTimer();
-};
-
-function countdownTimer() {
-    timeInterval = setInterval(function () {
-
-        if (timeLeft > 1) {
-            timerEl.textContent = "Time: " + timeLeft;
-            timeLeft--;
-        }
-        else {
-            timerEl.textContent = ' ';
-            clearInterval(timeInterval);
-            alert("TIMES UP!!");
-        }
-    }, 1000);
-};
-
-function setQuizQuestion() {
-    resetQuiz();
-    showQuestion(randomQuestions[currentQuestionIndex]);
-};
-
-function showQuestion(question) {
-    questionEl.innerText = question.question;
-    
-    question.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("btn");
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-        answerEl.appendChild(button);
-    })
-};
-
-function resetQuiz() {
-    while (answerEl.firstChild) {
-        answerEl.removeChild(answerEl.firstChild);
-    }
-};
-
-function selectAnswer(event) {
-    var selectedButton = event.target;
-    var correctAnswer = selectedButton.dataset.correct;
-    setStatus(document.body, correct);
-    Array.from(answerEl.children).forEach(button => {
-        setStatus(button, button.dataset.correct);
-    });
-
-    if(randomQuestions.length > currentQuestionIndex + 1) {
-
-    } else {
-        // show highscore
-    }
-};
-
-function setStatus(element, correct) {
-    clearSetStatus(element);
-    if (correct) {
-        element.classList.add("correct");
-    } else {
-        element.classList.add("wrong");
-    }
-};
-
-function clearSetStatus(element) {
-    element.classList.remove("correct");
-    element.classList.remove("wrong");
-}
 
 // Quiz Questions Object
 var questions = [
@@ -133,7 +54,7 @@ var questions = [
         ]
     },
     {
-        question: "A very useful took used during development and debugging for printing content to the debugger is:",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: [
             { text: "Javascript", correct: false },
             { text: "terminal/bash", correct: false },
@@ -142,3 +63,85 @@ var questions = [
         ]
     }
 ];
+startButtonEl.addEventListener("click", startGame)
+
+// Function that happens when the start quiz button is clicked and presents the questions
+function startGame() {
+    firstpageContainerEL.classList.add("hide");
+    questionContainerEl.classList.remove("hide");
+    setQuizQuestion();
+    countdownTimer();
+};
+
+// Timer Function that starts when the "start button is clicked"
+function countdownTimer() {
+    timeInterval = setInterval(function () {
+
+        if (timeLeft > 1) {
+            timerEl.textContent = "Time: " + timeLeft;
+            timeLeft--;
+        }
+        else {
+            timerEl.textContent = ' ';
+            clearInterval(timeInterval);
+            alert("TIMES UP!!");
+        }
+    }, 1000);
+};
+
+function setQuizQuestion() {
+
+    if (currentQuestionIndex < questions.length) {
+        resetQuiz();
+        showQuestion(questions[currentQuestionIndex]);
+    } else {
+        questionContainerEl.classList.add("hide");
+        responseEl.classList.add("hide");
+        highscoreEl.classList.remove("hide");
+        clearInterval(timeInterval);
+
+    }
+};
+
+function showQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement("button"); //create button for each answer
+        button.innerText = answer.text;
+        button.classList.add("btn");
+
+        if (answer.correct) { // check if answers:correct = true
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        answerEl.appendChild(button);
+    })
+};
+
+function resetQuiz() {
+    while (answerEl.firstChild) {
+        answerEl.removeChild(answerEl.firstChild);
+    }
+};
+
+function selectAnswer() {
+    var selectedButton = this.dataset.correct;
+
+    // if correct: true, then display "Correct"
+    if (selectedButton === "true") { //** NEED HELP */
+        timeLeft = timeLeft;
+        responseEl.setAttribute("style", "color:green");
+        responseEl.textContent = "Correct!"
+        score = score + 10;
+    } else {
+        responseEl.setAttribute("style", "color:red");
+        responseEl.textContent = "Incorrect";
+        timeLeft = timeLeft - 10;
+    }
+
+    currentQuestionIndex++;
+    setQuizQuestion();
+
+};
+
+
